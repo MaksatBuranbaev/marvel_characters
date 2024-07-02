22798d9d24c5efe998a4cbff668216c61b803aae
Maksat Buranbaev <zenfonemaxprom124@gmail.com>
2024-07-02 16:08:30 +0600

    add CharactersView

--- app/src/main/java/com/example/marvelharacters/MainActivity.kt
+++ app/src/main/java/com/example/marvelharacters/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.marvelharacters
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,7 +12,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.marvelharacters.ui.theme.MarvelСharactersTheme
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -20,12 +20,14 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -42,7 +44,11 @@ class MainActivity : ComponentActivity() {
                         modifier = Modifier
                             .fillMaxSize()
                             .padding(padding)
-                            /*.background(Color.Gray)*/){
+                            .paint(
+                                // Replace with your image id
+                                painterResource(id = R.drawable.background),
+                                contentScale = ContentScale.FillBounds
+                            )){
                         Row(modifier = Modifier
                             .fillMaxWidth()
                             /*.background(Color.Red)*/,
@@ -57,6 +63,7 @@ class MainActivity : ComponentActivity() {
                         {
                             Title()
                         }
+                        CharactersView(name = "Deadpool", img = R.drawable.images)
                     }
                 }
             }
@@ -81,6 +88,29 @@ private fun Title(){
         textAlign = TextAlign.Center,
         fontSize = 30.sp,
         fontWeight = FontWeight.Bold,
-        color = Color.White)
+        color = Color.White,
+        modifier = Modifier.padding(bottom = 32.dp))
 }
 
+@Composable
+private fun CharactersView(name: String, img: Int){
+    Row(modifier = Modifier
+        .fillMaxSize()
+        .padding(start = 40.dp, end = 40.dp, top = 20.dp, bottom = 60.dp)
+        .paint(
+            painterResource(id = img),
+            contentScale = ContentScale.FillBounds) ) {
+        Text(text = name, color = Color.White, fontSize = 32.sp, modifier = Modifier
+            .align(alignment = Alignment.Bottom)
+            .padding(start = 30.dp, bottom = 50.dp))
+    }
+}
+
+
+//class Character(val n: String, img: Int,)
+//{
+//    val name = n
+//    val image = img
+//}
+//
+//var characters = listOf(Character("Deadpool", R.drawable.images))
